075e5549e757be8ea3d8735819564177bb831cbd
Re0810 <khoo.junxiang2003@gmail.com>
2025-05-09 15:40:22 +0800

    Testing for bokking title

---
+++ lib/booking.dart
@@ -0,0 +1,22 @@
+// For testing purpose only
+import 'package:flutter/material.dart';
+
+class Booking extends StatelessWidget
+{
+  const Booking({super.key});
+
+  @override
+  Widget build(context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Booking',
+          style: TextStyle(
+            color: Colors.white12
+          ),
+        ),
+        backgroundColor: Colors.blueAccent
+      )
+    );
+  }
+}
