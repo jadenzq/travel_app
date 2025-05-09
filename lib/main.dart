07c2772ca8c79d58e5934c5e979b1bf61e84aeee
jadenzq <jadenzq@gmail.com>
2025-05-09 10:55:59 +0800

    Added lib folder to track

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:travel_app/homepage.dart';
+
+// editing here
+void main () {
+  runApp(
+    MaterialApp(
+      home: HomePage(),
+    )
+  );
+}
