602c39ffccd2c99b3834de61a1d7f2f06368aef7
KLsnake <zhangjunhao5051@gmail.com>
2025-06-30 15:31:23 +0800

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,55 @@
 import 'package:flutter/material.dart';
-import 'package:travel_app/app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:travel_app/Login/login.dart';
+import 'package:travel_app/Profile/profile.dart';
 
-// editing here
-void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: App()));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Travel App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const RootPage(),
+    );
+  }
+}
+
+class RootPage extends StatefulWidget {
+  const RootPage({super.key});
+
+  @override
+  State<RootPage> createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  @override
+  Widget build(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser;
+
+    // 用户已登录，进入 Profile 页面
+    if (user != null) {
+      return const ProfilePage();
+    }
+
+    // 用户未登录，进入 Login 页面
+    return LoginPage(
+      onLogIn: () {
+        if (!mounted) return;
+        setState(() {});
+      },
+    );
+  }
 }
